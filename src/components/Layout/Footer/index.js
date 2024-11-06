04270a3ed474c2af3cc4065f1d18eccac6d2a497
noman3671 <noman@geeksvisor.com>
2024-11-06 19:23:17 +0500

    remove unused code

--- src/components/Layout/Footer/index.js
+++ src/components/Layout/Footer/index.js
@@ -17,13 +17,6 @@ export const Footer = () => {
           <div className="footer_wrapper">
             <div className="footer_logo_main">
               <div className="mb-10 w-full mx-auto lg:mx-0 :!ml-0">
-                {/* <a href="/#" className="footer_logo">
-                  <img
-                    src={saddle_logo}
-                    alt="saddle_logo"
-                    className="w-[254.406px] h-auto"
-                  />
-                </a> */}
                 <a href="/#" className="footer_logo">
                   <img
                     src={saddle_logo}
