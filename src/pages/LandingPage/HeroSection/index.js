0b92facff6fec137fe6dd45aea11750075084dc2
noman3671 <noman@geeksvisor.com>
2024-11-06 19:37:31 +0500

    Remove Previous Code

--- src/pages/LandingPage/HeroSection/index.js
+++ src/pages/LandingPage/HeroSection/index.js
@@ -59,7 +59,7 @@ export const HeroSection = () => {
       className={`flex px-[22px] sm:px-2 xl:px-10 2xl:px-0 w-[95%] pb-[100px] xl:max-w-[1300px] mx-auto justify-normal`}
     >
       <div className="md:w-auto w-full md:mx-0 mx-auto items-center flex flex-col md:items-start">
-        {/* <div className="head_text flex md:text-start sm:text-center sm:flex-col md:flex-col m-0">
+        <div className="head_text flex md:text-start sm:text-center sm:flex-col md:flex-col m-0">
           <motion.span
             initial={{ opacity: 0, y: 20 }}
             animate={controlsScan}
@@ -79,35 +79,6 @@ export const HeroSection = () => {
               initial={{ opacity: 0, y: 20 }}
               animate={controlsPerform}
               className="block sm:leading-[90px] md:leading-[132px]"
-            >
-              {content_head_third}
-            </motion.span>
-          </div>
-        </div> */}
-        <div className="head_text flex md:text-start sm:text-center sm:flex-col md:flex-col m-0">
-          <motion.span
-            initial={{ opacity: 0, y: 20 }}
-            animate={controlsScan}
-            className="block sm:leading-[60px] md:leading-[90px]"
-            style={{ willChange: "transform, opacity" }}
-          >
-            {content_head_first}
-          </motion.span>
-
-          <div className="flex sm:flex-row md:flex-col">
-            <motion.span
-              initial={{ opacity: 0, y: 20 }}
-              animate={controlsFit}
-              className="block sm:leading-[60px] md:leading-[90px]"
-              style={{ willChange: "transform, opacity" }}
-            >
-              {content_head_second}
-            </motion.span>
-            <motion.span
-              initial={{ opacity: 0, y: 20 }}
-              animate={controlsPerform}
-              className="block sm:leading-[60px] md:leading-[90px]"
-              style={{ willChange: "transform, opacity" }}
             >
               {content_head_third}
             </motion.span>
