96c2baf141e8f716032326e051fa9d04618836c5
noman3671 <noman@geeksvisor.com>
2024-11-12 12:46:13 +0500

    minor fix

--- src/pages/LandingPage/FeatureCards/FeatureCardData.js
+++ src/pages/LandingPage/FeatureCards/FeatureCardData.js
@@ -11,31 +11,31 @@ export const featureCardData = [
   {
     title: "Find the Best Fitting Saddle",
     description:
-      "Scan your horse, then find the saddle that fits best through the SaddleFit online marketplace.",
+      "Scan your horse, then find the saddle that fits best through the SaddleFit online marketplace",
     icon: <SaddleIcon />,
   },
   {
     title: "Check the Fit of Your Saddle",
     description:
-      "Scan your horse and saddle to check the fit with SaddleFit’s innovative technology.",
+      "Scan your horse and saddle to check the fit with SaddleFit’s innovative technology",
     icon: <AlgorithmSaddleIcon />,
   },
   {
     title: "Improve Equine Performace",
     description:
-      "When hundredths of a second matter, give yourself the advantage with a properly fitting saddle.",
+      "When hundredths of a second matter, give yourself the advantage with a properly fitting saddle",
     icon: <SaddleTechIconSvg />,
   },
   {
     title: "Improve Equine Health",
     description:
-      "Reduce the changes of lameness and health issues with a properly fitting saddle.",
+      "Reduce the changes of lameness and health issues with a properly fitting saddle",
     icon: <HorseIcon />,
   },
   {
     title: "Technology Based Saddle Fitting",
     description:
-      "SaddleFit.io is based on 30+ years of saddle fitting experience and research.",
+      "SaddleFit.io is based on 30+ years of saddle fitting experience and research",
     icon: <MobileDevIcon/>,
   }
 ];
